e77eab661b811218e8ce1a2887ec04d7bded8381
Kimpeace <39222528+KimSnowFlower@users.noreply.github.com>
2024-09-03 23:58:34 +0900

    Add files via upload
    임시 업로드

---
+++ EV-main/src/main/java/Creeper/EV/Controller/TestController.java
@@ -0,0 +1,13 @@
+package Creeper.EV.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
