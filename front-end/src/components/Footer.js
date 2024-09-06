8a53ed18b1617b3b7f0534bec27a394275d7771a
o11117 <115409472+o11117@users.noreply.github.com>
2024-09-06 18:35:14 +0900

    Add files via upload

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,20 +1,19 @@
-import React from "react";
-import "./Footer.css";
-import logo4 from "../images/logo4.png";
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css';
+import logo from '../logo.svg';
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="footer">
-      <div className="contents">
-        <img src={logo4} width="60px" alt=""></img>
-        <h2 className="copyright">
-          COPYRIGHT © CREEPER. ALL RIGHTS RESERVED.
-          <br />
-          경기도 용인시 기흥구 강남로 40 강남대학교
-        </h2>
+    <footer>
+      <img src={logo} alt="Company Logo" className="footer-logo" />
+      <div className="footer-text">
+        <p>COPYRIGHT &copy; CREEPER. All RIGHTS RESERVED.</p>
+        <p>경기도 용인시 기흥구 강남로 40 강남대학교</p>
       </div>
+     
     </footer>
   );
-};
+}
 
 export default Footer;
