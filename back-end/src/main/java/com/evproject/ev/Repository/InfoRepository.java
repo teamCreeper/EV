586d8f60e72738eb2febf9446f7832f39e796a8a
KimSnowFlower <kimbona1148@gmail.com>
2024-09-09 23:53:48 +0900

    20240909

--- back-end/src/main/java/com/evproject/ev/Repository/InfoRepository.java
+++ back-end/src/main/java/com/evproject/ev/Repository/InfoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InfoRepository extends JpaRepository<InfoEntity, Long> {
+public interface InfoRepository extends JpaRepository<InfoEntity, String> {
 }
